d16cefe3e0cc0e53696d0867406745b4f7c622a8
nicknatali <nicka.natali@calbaptist.edu>
2017-04-12 22:32:21 +0000

    Wrapping up the project, finishing user details, cleaning up bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,4 +32,9 @@ class UsersController < ApplicationController
         
     end 
     
+    def show 
+        @user = User.find(params[:id])
+        @user_stocks = @user.stocks
+    end 
+    
 end 
